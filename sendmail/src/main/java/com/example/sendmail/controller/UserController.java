f2889b595412a49484c4c6fa3a7a253b07b3792e
lwy123-wq <l2158223895@qq.com>
2021-12-05 22:10:18 +0800

    mail

--- sendmail/src/main/java/com/example/sendmail/controller/UserController.java
+++ sendmail/src/main/java/com/example/sendmail/controller/UserController.java
@@ -33,11 +33,9 @@ public class UserController {
         for(int i=0;file.length>i;i++) {
             // 获取文件名称,包含后缀
             String fileName = file[i].getOriginalFilename();
-
             // 存放在这个路径下：该路径是该工程目录下的static文件下：(注：该文件可能需要自己创建)
             // 放在static下的原因是，存放的是静态文件资源，即通过浏览器输入本地服务器地址，加文件名时是可以访问到的
             String path = "/home/lwy/文档/lwy";
-//			String path = ClassUtils.getDefaultClassLoader().getResource("").getPath()
             try {
                 // 该方法是对文件写入的封装，在util类中，导入该包即可使用
                 long t = System.currentTimeMillis();// 获得当前系统毫秒数,
@@ -71,9 +69,6 @@ public class UserController {
         if(!file.isEmpty()) {
             // 获取文件名称,包含后缀
             String fileName = file.getOriginalFilename();
-
-            // 存放在这个路径下：该路径是该工程目录下的static文件下：(注：该文件可能需要自己创建)
-            // 放在static下的原因是，存放的是静态文件资源，即通过浏览器输入本地服务器地址，加文件名时是可以访问到的
             String path = "/home/lwy/文档/lwy";
 //			String path = ClassUtils.getDefaultClassLoader().getResource("").getPath()
             try {
